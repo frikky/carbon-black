dec88af0aae87e916b458d84623b1f8407c4a00b
frikky <fredrik_9490@hotmail.com>
2017-10-16 10:48:48 -0400

    ¯\_(ツ)_/¯

--- memdump.py
+++ memdump.py
@@ -6,6 +6,7 @@ import time
 import requests
 from cb import sensorhandler 
 from exfil import exfildir
+from time import gmtime, strftime 
 
 class dumpmemory(object):
     def __init__(self, computername): 
@@ -14,17 +15,21 @@ class dumpmemory(object):
         self.sensorhandler = sensorhandler()
         sensordata = self.sensorhandler.get_sensordata(self.computername)
         self.session = self.sensorhandler.find_session(sensordata)
+        self.directory = "C:\\ProgramData\\"
+
+    def get_time(self):
+        return strftime("%H:%M:%S", gmtime())
 
     # Returns whether there is enough diskspace to do a memdump. 
     # Based on: diskspace > (memory*2+5gb~)
-    def check_diskspace(self, username):
+    def check_diskspace(self):
         # Only works for windows (powershell)
         # FIX - Remove hardcoded stuff path and add unix stuff. Might also need to check other disks
         
         # This command measures remaining disk space and max memory size
         powershell = "powershell.exe \"(gwmi win32_logicaldisk | Where-Object {$_.DeviceID -eq \'C:\'}).freespace; (gwmi win32_physicalmemory | Measure Capacity -Sum).sum\""
 
-        output_file = "C:\Users\\%s\AppData\Local\\temp\\cbdata.txt" % username
+        output_file = "%scbdata.txt" % self.directory
             
         powershellret = self.start_new_process(self.session, \
             command="create process", curobject=powershell, output_file=output_file)
@@ -35,9 +40,9 @@ class dumpmemory(object):
         else:
             exit()
 
-        # Waits for file to be fully created
         time.sleep(2)
 
+        # Waits for file to be fully created
         # Grabs the filedata (e.g. file_id)
         fileret = self.start_new_process(
             self.session, 
@@ -51,6 +56,8 @@ class dumpmemory(object):
             fileret, 
             output_file
         )
+
+        time.sleep(2)
 
         # Deletes the file
         self.start_new_process(
@@ -70,26 +77,26 @@ class dumpmemory(object):
         sizeoffset = 5000000000
 
         if (diskspace-(memory*2)) < sizeoffset:
-            print "Might not be enough space on disk. Only %d left, but memory is %d" \
-                % (diskspace, memory)
+            print "%s: Might not be enough space on disk. Only %d left, but memory is %d" \
+                % (self.get_time(), diskspace, memory)
             return False
             
         return True
 
     def create_multiple_folders(self, path):
+        if not path.endswith("\\"): 
+            path += "\\"
+
         if not os.path.isdir(path):
-            print "Creating path %s" % path
+            print "%s: Creating path %s" % (self.get_time(), path)
             os.makedirs(path) 
 
     # Gets the actual memory
-    def get_memory(self, username):
-        output_location = "C:\\Users\\%s\\AppData\\Local\\temp\\dump" % username
-        compress_check=False
+    def get_memory(self):
+        output_location = "%smemdump.dmp" % self.directory
+        compress_check=True
 
         #Runs the memdump process - This takes a while if compress_check is True
-        # 17~ min on 17gb~ dump
-        # Fix - uncomment this
-        """
         fileret = self.start_new_process(
             self.session, 
             command="memdump", 
@@ -97,27 +104,21 @@ class dumpmemory(object):
             compress=compress_check
         )
 
-        if fileret.startswith("Command not supported"):
-            print "Sensor version is < 5.1 and cannot be executed properly."
-            exit()
+        try:
+            if fileret.startswith("Command not supported"):
+                print "%s: Sensor version is < 5.1 and cannot be executed properly." % self.get_time()
+                exit()
+        except AttributeError:
+            pass
 
-        print fileret
-        """
-
-        if compress_check:
+        # If compressing is done on the remote host, the original dump will be deleted.
             # Deletes the dump file 
-            self.start_new_process(
-                self.session, 
-                command="delete file", 
-                curobject=output_location
-            )
 
-            # Overrides to download 
-
-        output_location = output_location+".zip"
         exfil = exfildir("", self.computername)
 
-        print "or Here?"
+        if compress_check:
+            output_location = output_location+".zip"
+
         new_commanddata = self.start_new_process(\
             self.session, 
             command="get file", 
@@ -130,13 +131,25 @@ class dumpmemory(object):
             "data/%s/dumpfile/" % self.computername
         )
 
+        # Sleeps because errors with file deletion might occur while file is in use
+        time.sleep(5)
+
         self.start_new_process(
             self.session, 
             command="delete file", 
             curobject=output_location
         )
 
-        
+        if compress_check:
+            output_location = output_location[:-4]
+            self.start_new_process(
+                self.session, 
+                command="delete file", 
+                curobject=output_location
+            )
+
+        return True
+
         """
         Response Object
 
@@ -146,9 +159,6 @@ class dumpmemory(object):
         percentdone: percent of the process completed
         dumping: boolean flag indicating if memory dump is in progress
         """
-
-
-        # Returns the data in the file
 
     # Doesn't save data, but returns it raw
     def read_data(self, session_id, commanddata, path):
@@ -171,14 +181,14 @@ class dumpmemory(object):
 
     # Runs commands on the Carbon black endpoint
     def start_new_process(self, session, command="create process", \
-        curobject="", wait="", output_file="", compress=""):
+        curobject="", wait="", output_file="", compress=True):
 
         curid = session["id"]
         urlpath = "/api/v1/cblr/session/%s/command" % curid
 
         data = {"session_id": curid, "name": command, "object": curobject}
-        print "Running command \'%s\' with argument %s" % \
-            (command, curobject)
+        print "%s: Running command \'%s\' with argument %s" % \
+            (self.get_time(), command, curobject)
 
         # Appends stuff
         if output_file:
@@ -198,25 +208,28 @@ class dumpmemory(object):
 
         # Should never happen, but its too be sure
         if not ret.status_code == 200:
-            sys.stdout.write("Couldn't connect to the endpoint. Raw error:\n")
-            print ret.text
+            sys.stdout.write("%s: Couldn't connect to the endpoint. Raw error:\n%s" % (self.get_time(), ret.text))
             exit()
 
         # Verifies if the command is finished or not.
         commanddata = ""
         refreshcnt = 0
         while(1):
+            # Creates a request to verify whether the command is finished or not
             urlpath = "/api/v1/cblr/session/%s/command/%d" % (curid, ret.json()["id"])
             commandret = requests.get(
                 "%s%s" % (self.sensorhandler.url, urlpath), 
                 headers=self.sensorhandler.header, 
                 verify=False
             )
-
+                
+            # Exits if an error occurs. This means the command was injected badly.
             if commandret.json()["status"] == "error":
-                print "An error occurred while issuing the command. Raw:\n%s" % commandret.json()
+                # Errors in delete file..
+                print "%s: An error occurred while issuing the command. Raw:\n%s" % (self.get_time(), commandret.json())
                 exit()
 
+            # Returns the commanddata if the command is finished
             if commandret.json()["status"] == "complete":
                 commanddata = commandret.json()
                 break    
@@ -224,17 +237,18 @@ class dumpmemory(object):
             # Hardcoded for memdump
             # Should maybe give % finished?
             if command == "memdump" and compress: 
-                sys.stdout.write("Memdump takes a long time when compressing. Checking if complete ever 60 seconds.\n")
+                if refreshcnt is 0:
+                    sys.stdout.write("%s: Memdump takes a long time when compressing. Checking if complete every 60 seconds. Raw:\n%s\n" % (self.get_time(), commandret.json()))
                 time.sleep(60)
             else:
-                sys.stdout.write("Command not finished, refreshing every 5 seconds.\n")
-                time.sleep(5)
-
-            if refreshcnt > 10:
-                print "If you're running \'getfile\' on a large item it will take a long ass time. Raw:%s" % commandret.json()
-                refreshcnt = 0
+                if refreshcnt is 0:
+                    sys.stdout.write("%s: Checking whether the command is finished every 10 seconds. Raw: \n%s\n" % (self.get_time(), commandret.json()))
+                time.sleep(10)
 
             refreshcnt += 1
+
+            if refreshcnt > 10:
+                refreshcnt = 0
 
         return commanddata
 
@@ -246,10 +260,13 @@ if __name__ == "__main__":
     computername = sys.argv[1]
     dump = dumpmemory(computername)
 
-    # Hardcoded for now
-    username = "fodeg"
-    diskcheck = dump.check_diskspace(username)
+    diskcheck = dump.check_diskspace()
     if not diskcheck:
         exit()
 
-    memdumpcheck = dump.get_memory(username)
+    memdumpcheck = dump.get_memory()
+    if memdumpcheck:
+        print "Dump should now be available in the data/%s/dumpfile/ folder." % computername
+    else:
+        print "Something went wrong during memdump?"
+
